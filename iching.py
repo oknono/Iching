08738f3b91357219bc4454288d5f245c20de31b8
Kirsten <kirsten@eml.cc>
2014-11-03 15:23:56 +0700

    First commit, or second

---
+++ iching.py
@@ -0,0 +1,15 @@
+# I-ching first setup
+# Last edited: 3 nov 2014
+# By kirsten - oknono@github - kirsten@eml.cc
+
+# Ask user to throw three coins
+# Coin Toss by pressing enter. 
+# After 3 enter don't read input anymore
+
+# After three tosses, draw line and ask for three more. Repeat until we have 6 lines
+
+# Show the hexagram by drawing line 6 on top and line 1 on bottom
+
+# Show number of Hexagram
+
+# Provide link to text or get text from a website (open source text)N
